d200cc4d00c3a1983dc53f837d632586a46bbbf2
dinssa <manny@dinssa.com>
2023-06-01 00:51:00 +0100

    Connect to the database with mongoose

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const session = require('express-session');
 
 require('dotenv').config();
+require('./config/database');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
