37a79a520342f97f8c19803c0ac2211d4923882d
dinssa <manny@dinssa.com>
2023-06-05 23:43:19 +0100

    Install method overide

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const methodOveride= require('method-override');
 
 const session = require('express-session');
 const passport = require('passport');
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOveride('_method'));
 
 app.use(session({
   secret: process.env.SECRET,
